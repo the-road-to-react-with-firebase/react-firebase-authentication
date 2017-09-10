f54adbdfdf3233a0bb6f57196ca2e154c901eeb6
Robin Wieruch <wrobin@gmx.net>
2017-09-10 09:08:28 +0200

    finish firebase and app boilerplate, but not connected

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import SignUpLink from '../SignUp';
-import PasswordForgetLink from '../PasswordForget';
+import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
 
 const SignInPage = () =>
   <div>
