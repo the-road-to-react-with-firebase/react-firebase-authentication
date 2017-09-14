9af50f1c0c6db245271d017820221724206917ec
Robin Wieruch <wrobin@gmx.net>
2017-09-14 04:32:57 +0200

    extend footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -32,7 +32,7 @@ const App = () =>
 
       <hr/>
 
-      <p>Found in <a href="https://roadtoreact.com/course-details?courseId=TAMING_THE_STATE">Taming the State in React</a></p>
+      <span>Found in <a href="https://roadtoreact.com/course-details?courseId=TAMING_THE_STATE">Taming the State in React</a></span> | <span>Star the <a href="https://github.com/rwieruch/react-firebase-authentication">Repository</a></span> | <span>Receive a <a href="https://www.getrevue.co/profile/rwieruch">Developer's Newsletter</a></span>
     </div>
   </Router>
 
