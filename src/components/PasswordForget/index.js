d68754d326623ce3d02ab87b22835e52b3948507
Robin Wieruch <wrobin@gmx.net>
2017-12-08 11:52:55 +0800

    add password forget route constant

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase';
+import * as routes from '../../constants/routes';
 
 const PasswordForgetPage = () =>
   <div>
@@ -67,7 +68,7 @@ class PasswordForgetForm extends Component {
 
 const PasswordForgetLink = () =>
   <p>
-    <Link to="/pw-forget">Forgot Password?</Link>
+    <Link to={routes.PASSWORD_FORGET}>Forgot Password?</Link>
   </p>
 
 export default PasswordForgetPage;
