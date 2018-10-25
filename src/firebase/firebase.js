406fb142df989dc770c4492dd06f911ee3ca21e6
Robin Wieruch <wrobin@gmx.net>
2018-10-25 12:04:43 +0800

    add env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,21 +3,21 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const prodConfig = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTH_DOMAIN,
-  databaseURL: YOUR_DATABASE_URL,
-  projectId: YOUR_PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+databaseURL: process.env.REACT_APP_DATABASE_URL,
+projectId: process.env.REACT_APP_PROJECT_ID,
+storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 const devConfig = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTH_DOMAIN,
-  databaseURL: YOUR_DATABASE_URL,
-  projectId: YOUR_PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+databaseURL: process.env.REACT_APP_DATABASE_URL,
+projectId: process.env.REACT_APP_PROJECT_ID,
+storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 const config = process.env.NODE_ENV === 'production'
