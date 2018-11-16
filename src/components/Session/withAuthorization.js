8256424618fb202212e8faf564ae6afdbf0ef7dc
Robin Wieruch <wrobin@gmx.net>
2018-11-16 08:51:15 +0800

    merge dbuser and authuser in authorization HOC

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import AuthUserContext from './AuthUserContext';
-import { firebase } from '../../firebase';
+import { firebase, db } from '../../firebase';
 import * as ROUTES from '../../constants/routes';
 
 const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
     componentDidMount() {
       this.listener = firebase.auth.onAuthStateChanged(authUser => {
-        if (!condition(authUser)) {
-          this.props.history.push(ROUTES.SIGN_IN);
+        if (authUser) {
+          db.onceGetUser(authUser.uid).then(snapshot => {
+            let dbUser = snapshot.val();
+
+            if (!dbUser.roles) {
+              dbUser.roles = [];
+            }
+
+            authUser = { ...authUser, ...dbUser };
+
+            if (!condition(authUser)) {
+              this.props.history.push(ROUTES.SIGN_IN);
+            }
+          });
         }
       });
     }
@@ -23,7 +35,7 @@ const withAuthorization = condition => Component => {
       return (
         <AuthUserContext.Consumer>
           {authUser =>
-            authUser ? <Component {...this.props} /> : null
+            condition(authUser) ? <Component {...this.props} /> : null
           }
         </AuthUserContext.Consumer>
       );
