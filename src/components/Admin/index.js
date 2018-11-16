d0100221dfb7df503b855de564b3d8733ed7a7a0
Robin Wieruch <wrobin@gmx.net>
2018-11-16 09:00:09 +0800

    protect admin page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import * as ROLES from '../../constants/roles';
+import AuthUserContext from '../Session/AuthUserContext';
+import withAuthorization from '../Session/withAuthorization';
+
 const AdminPage = () => (
-  <div>
-    <h1>Admin Page</h1>
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Admin Account: {authUser.email}</h1>
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AdminPage;
+const authCondition = authUser =>
+  authUser && authUser.roles.includes(ROLES.ADMIN);
+
+export default withAuthorization(authCondition)(AdminPage);
