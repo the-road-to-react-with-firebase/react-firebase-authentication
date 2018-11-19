22a83a6efdff36f3bb18c5f993d8b37ed8b5c63f
Robin Wieruch <wrobin@gmx.net>
2018-11-19 09:06:57 +0800

    upgrade service worker to CRA 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
@@ -14,4 +14,7 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
