65e6c18fb894b847fed0ab6f9bb042b310cfedc2
Robin Wieruch <wrobin@gmx.net>
2018-11-19 19:58:54 +0800

    02-Firebase in React

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
