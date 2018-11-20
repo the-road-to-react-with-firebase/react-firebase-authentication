5cef7624455f585df4af38f6e04b6d418d82a388
Robin Wieruch <wrobin@gmx.net>
2018-11-20 11:14:44 +0800

    07-Sign Out with React and Firebase

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
@@ -20,6 +21,9 @@ const Navigation = () => (
       </li>
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+      <li>
+        <SignOutButton />
       </li>
     </ul>
   </div>
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const SignOut = () => (
-  <div>
-    <h1>SignOut</h1>
-  </div>
+import { withFirebase } from '../Firebase';
+
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
 );
 
-export default SignOut;
+export default withFirebase(SignOutButton);
