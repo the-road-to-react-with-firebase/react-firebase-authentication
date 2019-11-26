bdd1a804ff58d3c4c104ff3bcd69d045777f9c94
Tim van Mourik <timvanmourik@gmail.com>
2019-11-26 18:40:12 +0100

    hookify passwordforget and use formik

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -13,57 +14,51 @@ const PasswordForgetPage = () => (
 
 const INITIAL_STATE = {
   email: '',
-  error: null,
 };
 
-class PasswordForgetFormBase extends Component {
-  constructor(props) {
-    super(props);
+const PasswordForgetFormBase = (props) => {
+  const onSubmit = (values, { setFieldError, setSubmitting }) => {
+    const {email} = values;
 
-    this.state = { ...INITIAL_STATE };
-  }
-
-  onSubmit = event => {
-    const { email } = this.state;
-
-    this.props.firebase
+    props.firebase
       .doPasswordReset(email)
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
       })
       .catch(error => {
-        this.setState({ error });
+        setFieldError('general', error.message);
       });
+  }
 
-    event.preventDefault();
-  };
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  }
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    const { email, error } = this.state;
-
-    const isInvalid = email === '';
-
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={this.state.email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
+  return (
+    <Formik
+      initialValues={INITIAL_STATE}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+    {({ isSubmitting, errors }) => (
+      <Form>
+        <Field type="email" name="email" placeholder="Email Address"/>
+        <ErrorMessage name="email" component="div" />
+        <button disabled={isSubmitting} type="submit">
           Reset My Password
         </button>
-
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+          <span style={{ color: 'red' }}>{errors.general}</span>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 const PasswordForgetLink = () => (
