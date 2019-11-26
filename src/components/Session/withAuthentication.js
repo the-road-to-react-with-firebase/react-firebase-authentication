0ed0ec989eb22d89aa7693b278b9f5f037a116cd
Tim van Mourik <timvanmourik@gmail.com>
2019-11-26 18:01:29 +0100

    hookify `withAuthentication`

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,42 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
-  class WithAuthentication extends React.Component {
-    constructor(props) {
-      super(props);
+  const WithAuthentication = (props) => {
+    const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem('authUser')));
 
-      this.state = {
-        authUser: JSON.parse(localStorage.getItem('authUser')),
-      };
-    }
-
-    componentDidMount() {
-      this.listener = this.props.firebase.onAuthUserListener(
+    useEffect(() => {
+      const unsubscribe = props.firebase.onAuthUserListener(
         authUser => {
           localStorage.setItem('authUser', JSON.stringify(authUser));
-          this.setState({ authUser });
+          setAuthUser(authUser);
         },
         () => {
           localStorage.removeItem('authUser');
-          this.setState({ authUser: null });
+          setAuthUser(null);
         },
       );
-    }
+      return () => unsubscribe();
+    })
 
-    componentWillUnmount() {
-      this.listener();
-    }
-
-    render() {
-      return (
-        <AuthUserContext.Provider value={this.state.authUser}>
-          <Component {...this.props} />
-        </AuthUserContext.Provider>
-      );
-    }
+    return (
+      <AuthUserContext.Provider value={authUser}>
+        <Component {...props} />
+      </AuthUserContext.Provider>
+    );
   }
 
   return withFirebase(WithAuthentication);
